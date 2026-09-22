2901d1eb5930567e95f953b58caf056a36d488c6
SajidBaloch123 <sajidbbaloch69@gmail.com>
2026-09-23 00:56:06 +0500

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Sajid-Portfolio-/',    // ✅ Exact match
+  base: '/Sajid-Portfolio-/',
   publicDir: 'public',
 })
