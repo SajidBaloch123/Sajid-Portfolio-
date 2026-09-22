2b79d24d309b82c3501cc96afd5c35815e8a517e
SajidBaloch123 <sajidbbaloch69@gmail.com>
2026-09-23 00:52:42 +0500

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/sajid-portfolio/',
+  base: '/Sajid-Portfolio-/',    // ✅ Exact match
   publicDir: 'public',
 })
