8686c356fc729db5733653ccc1c571a4fc95489d
SajidBaloch123 <sajidbbaloch69@gmail.com>
2026-09-23 00:26:34 +0500

    Change base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Professional-Portfolio/',    // ✅ Aapke repo ka exact naam
+  base: '/sajid-portfolio/',
   publicDir: 'public',
 })
